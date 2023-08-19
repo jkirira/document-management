860e997afd4f9f92ebfb5b6dfe79b4ff890a56cd
jkirira <jkirira@cytonn.com>
2023-08-19 23:53:23 +0300

    Adding users controoler

---
+++ app/Http/Controllers/Api/v1/UsersController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\UserRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class UsersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function index()
+    {
+        $this->authorize('viewAny', User::class);
+        return response()->json(User::all(), Response::HTTP_OK);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function create(UserRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function store(Request $request)
+    {
+        $this->authorize('create', User::class);
+
+        $user = User::create($request->validated());
+        return response()->json([], Response::HTTP_CREATED);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $user = User::findOrFail($id);
+
+        $this->authorize('view', $user);
+
+        return response()->json($user, Response::HTTP_CREATED);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function update(UserRequest $request, $id)
+    {
+        $user = User::findOrFail($id);
+
+        $this->authorize('update', $user);
+
+        $user = $user->update($request->validated());
+        return response()->json([], Response::HTTP_CREATED);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $user = User::findOrFail($id);
+
+        $this->authorize('delete', $user);
+
+        $user->delete();
+        return response()->json([], Response::HTTP_OK);
+    }
+}
--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -64,7 +64,7 @@ class UserPolicy
      */
     public function delete(User $user, User $model)
     {
-        return $user->isAdmin();
+        return $user->isAdmin() && $user->id !== $model->id;
     }
 
     /**
@@ -76,7 +76,7 @@ class UserPolicy
      */
     public function restore(User $user, User $model)
     {
-        return $user->isAdmin();
+        return $user->isAdmin() && $user->id !== $model->id;
     }
 
     /**
@@ -88,6 +88,6 @@ class UserPolicy
      */
     public function forceDelete(User $user, User $model)
     {
-        return $user->isAdmin();
+        return $user->isAdmin() && $user->id !== $model->id;
     }
 }
