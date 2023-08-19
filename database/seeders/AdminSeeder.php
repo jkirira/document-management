ce293b699ff2f1e1bbc3648ec894f54bbd27d872
jkirira <jkirira@cytonn.com>
2023-08-19 22:33:03 +0300

    adding admin seeder.

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $adminRole = Role::updateOrCreate(
+                        ['slug' => 'admin'],
+                        ['name' => 'admin']
+                    );
+
+        $adminUser = User::updateOrCreate(
+                        ['email' => 'admin@admin.com'],
+                        [
+                            'name' => 'admin',
+                            'password' => Hash::make('password')
+                        ]
+                    );
+
+        $adminUser->roles()->attach($adminRole->id);
+
+    }
+}
