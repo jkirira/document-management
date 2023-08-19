db527afd0ccfefda0321d82cfddbddf8eb72926e
jkirira <jkirira@cytonn.com>
2023-08-19 23:46:57 +0300

    Add soft deletes trait to all models

--- app/Models/DepartmentAccess.php
+++ app/Models/DepartmentAccess.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class DepartmentAccess extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $table = 'document_department_access';
 
