e79be4a2c135f86018fa0b35bbfda92b4e4dd15b
jkirira <jkirira@cytonn.com>
2023-08-29 13:51:01 +0300

    Create document access model.

---
+++ app/Models/DocumentAccess.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class DocumentAccess extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'document_access';
+
+    protected $guarded = [
+        'id',
+    ];
+
+
+    const ACCESS_TYPES = [
+        'update',
+        'view',
+        'delete',
+        'download',
+    ];
+
+
+    public function document()
+    {
+        return $this->belongsTo(Document::class, 'document_id');
+    }
+
+    public function department()
+    {
+        return $this->belongsTo(Department::class, 'department_id');
+    }
+
+    public function role()
+    {
+        return $this->belongsTo(Role::class, 'role_id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function grantedBy()
+    {
+        return $this->belongsTo(User::class, 'granted_by');
+    }
+
+    public function revokedBy()
+    {
+        return $this->belongsTo(User::class, 'revoked_by');
+    }
+
+    public function accessRequest()
+    {
+        return $this->belongsTo(AccessRequest::class, 'access_request_id');
+    }
+
+}
