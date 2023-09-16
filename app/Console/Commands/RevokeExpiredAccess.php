06ccb9838560a06ff7e60a8979ed29751e09151c
jkirira <jkirira@cytonn.com>
2023-09-16 22:10:01 +0300

    Implementing cron to handle expired accesses.

---
+++ app/Console/Commands/RevokeExpiredAccess.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\DocumentAccess;
+use App\Notifications\DocumentAccessExpired;
+use App\Services\DocumentAccessService;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use PhpParser\Comment\Doc;
+
+class RevokeExpiredAccess extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'access:revoke-expired {--after=}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Revoke Access that have expired';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $this->info('Starting command RevokeExpiredAccess');
+
+        $after = $this->option('after') ?? Carbon::now();
+
+        $expiredAccesses = DocumentAccess::with(['user', 'document'])
+                                        ->where('expires_at', '<=', $after)
+                                        ->get();
+
+        $documentAccessService = new DocumentAccessService();
+
+        foreach ($expiredAccesses as $expiredAccess) {
+            $this->info('Access id - ' . $expiredAccess->id);
+
+            try {
+
+                $expiredAccess = DB::transaction(function() use ($documentAccessService, $expiredAccess) {
+                    $expiredAccess->update(['expired' => true]);
+                    return $documentAccessService->revokeAccess($expiredAccess);
+                });
+
+                $expiredAccess->refresh();
+
+                $user = $expiredAccess->user;
+                if (isset($user)) {
+                    $user->notify(new DocumentAccessExpired($expiredAccess));
+                }
+
+            } catch (\Exception $exception) {
+                if (!$expiredAccess->fresh()->expired) {
+                    $this->error('Revoke Access id - ' . $expiredAccess->id . 'failed');
+                } else {
+                    $this->error('Revoke Access id - ' . $expiredAccess->id . 'something went wrong');
+                }
+            }
+
+        }
+
+        $this->info('Stopped command RevokeExpiredAccess');
+
+    }
+}
---
+++ app/Notifications/DocumentAccessExpired.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\DocumentAccess;
+use Carbon\Carbon;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class DocumentAccessExpired extends Notification
+{
+    use Queueable;
+
+    protected $documentAccess;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @param DocumentAccess $documentAccess
+     */
+    public function __construct(DocumentAccess $documentAccess)
+    {
+        $this->documentAccess = $documentAccess;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        $document = $this->documentAccess->document;
+
+        return (new MailMessage)
+                            ->subject('Document Access Request Expired')
+                            ->greeting('Hello!')
+                            ->line('Your access to ' .
+                                        ($document ? $document->name : '') .
+                                        ' expired at ' .
+                                        Carbon::parse($this->documentAccess->expires_at)->toDayDateTimeString()
+                            )
+                            ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
