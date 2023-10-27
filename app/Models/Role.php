0f27ed428988b76dedf98f3ca64554864041176c
jkirira <jkirira@cytonn.com>
2023-10-27 13:31:01 +0300

    Installing Searchable package.

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -5,10 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Nicolaslopezj\Searchable\SearchableTrait;
 
 class Role extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory, SoftDeletes, SearchableTrait;
 
     protected $table = 'roles';
 
@@ -16,8 +17,10 @@ class Role extends Model
         'id',
     ];
 
-    protected $searchColumns = [
-        'name',
+    protected $searchable = [
+        'columns' => [
+            'name' => 1,
+        ],
     ];
 
     const ADMIN_ROLE_SLUG = 'admin';
@@ -28,19 +31,23 @@ class Role extends Model
         return $this->belongsToMany(User::class, 'role_users', 'role_id', 'user_id');
     }
 
-    public function scopeSearch($query, string $terms = null)
-    {
-        $columns = array_filter($this->searchColumns);
-        $terms = array_filter(explode(' ', $terms));
+    /*
+    * CUSTOM SEARCH
+    */
+    // public function scopeSearch($query, string $terms = null)
+    // {
+    //     $columns = array_filter($this->searchColumns);
+    //     $terms = array_filter(explode(' ', $terms));
 
-        $query->where(function ($query) use ($terms, $columns) {
-            foreach ($columns as $column) {
-                $query->orWhere(function ($query) use ($terms, $column) {
-                    foreach ($terms as $term) {
-                        $query->orWhere($column, 'like', '%'.$term.'%');
-                    };
-                });
-            };
-        });
-    }
+    //     $query->where(function ($query) use ($terms, $columns) {
+    //         foreach ($columns as $column) {
+    //             $query->orWhere(function ($query) use ($terms, $column) {
+    //                 foreach ($terms as $term) {
+    //                     $query->orWhere($column, 'like', '%'.$term.'%');
+    //                 };
+    //             });
+    //         };
+    //     });
+    // }
+
 }
