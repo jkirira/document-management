7b92039a053a0e6a062445df6121928de3e31d57
jkirira <jkirira@cytonn.com>
2024-02-23 15:41:35 +0300

    Granting user access when user creates a document.

--- app/Services/DocumentService.php
+++ app/Services/DocumentService.php
@@ -4,8 +4,10 @@
 namespace App\Services;
 
 use App\Models\Document;
+use App\Models\DocumentAccess;
 use App\Models\Folder;
 use App\Models\UserCategory;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
@@ -20,52 +22,74 @@ class DocumentService
 
     public function addNewDocument($file, $input, $owner = null)
     {
-        $folder = isset($input['folder_id']) ? Folder::find($input['folder_id']) : null;
-        $storage_location = isset($folder) ? $this->generateStorageLocation($folder) : config('constants.documents.storage_folder', '');
-        $path = $file->store($storage_location, 'public');
-        if (!$path) {
-            throw \Exception::class('Could not add document');
-        }
+        return DB::transaction(function () use ($file, $input, $owner) {
 
-        $document = Document::create([
-            "name" => $input['name'],
-//            "filename" => $file->extension(),
-            "extension" => $file->extension(),
-            "path" => $path,
-            'added_by' => isset($owner) ? $owner->id : auth()->user()->id,
-            'folder_id' => $folder ? $folder->id : null,
-        ]);
+            $folder = isset($input['folder_id']) ? Folder::find($input['folder_id']) : null;
+            $storage_location = isset($folder) ? $this->generateStorageLocation($folder) : config('constants.documents.storage_folder', '');
+            $path = $file->store($storage_location, 'public');
+            if (!$path) {
+                throw \Exception::class('Could not add document');
+            }
 
-        if (isset($input['category_ids'])) {
-            $categories = UserCategory::whereIn('id', $input['category_ids'])->get();
-            $document->categories()->sync($categories->pluck('id'));
-        }
+            $document = Document::create([
+                "name" => $input['name'],
+    //            "filename" => $file->extension(),
+                "extension" => $file->extension(),
+                "path" => $path,
+                'added_by' => isset($owner) ? $owner->id : auth()->user()->id,
+                'folder_id' => $folder ? $folder->id : null,
+            ]);
 
-        return $document;
+
+            // Grant user document access
+            $accessDetails = [];
+            $accessDetails['user_id'] = auth()->user()->id;
+            foreach (DocumentAccess::ACCESS_ABILITIES as $ability) {
+                $accessDetails[$ability] = true;
+            }
+            (new DocumentAccessService())->grantAccess($document, $accessDetails);
+
+
+            // Add user to access managers
+            $document->accessManagers()->attach(auth()->user()->id);
+
+            // Add document categories
+            if (isset($input['category_ids'])) {
+                $categories = UserCategory::whereIn('id', $input['category_ids'])->get();
+                $document->categories()->sync($categories->pluck('id'));
+            }
+
+            return $document;
+        });
+
     }
 
-    public function updateDocument(Document $document, $input) {
-        $folder = isset($input['folder_id']) ? Folder::find($input['folder_id']) : null;
-        if(isset($folder) && ($folder->id !== $document->folder_id)) {
-            // move document
-            $storage_location = $this->generateStorageLocation($folder);
-            Storage::move($document->path, $storage_location);
+    public function updateDocument(Document $document, $input)
+    {
+        return DB::transaction(function () use ($document, $input) {
 
-            $document->path = $storage_location;
-            $document->folder_id = $folder->id;
-        }
+            $folder = isset($input['folder_id']) ? Folder::find($input['folder_id']) : null;
+            if(isset($folder) && ($folder->id !== $document->folder_id)) {
+                // move document
+                $storage_location = $this->generateStorageLocation($folder);
+                Storage::move($document->path, $storage_location);
 
-        $document->name = $input['name'];
-//        $document->filename = $file->extension();
-//        $document->extension = $file->extension();
-        $document->save();
+                $document->path = $storage_location;
+                $document->folder_id = $folder->id;
+            }
 
-        if (isset($input['category_ids'])) {
-            $categories = UserCategory::whereIn('id', $input['category_ids'])->get();
-            $document->categories()->sync($categories->pluck('id'));
-        }
+            $document->name = $input['name'];
+    //        $document->filename = $file->extension();
+    //        $document->extension = $file->extension();
+            $document->save();
 
-        return $document;
+            if (isset($input['category_ids'])) {
+                $categories = UserCategory::whereIn('id', $input['category_ids'])->get();
+                $document->categories()->sync($categories->pluck('id'));
+            }
+
+            return $document;
+        });
     }
 
     public function generateStorageLocation(Folder $folder) {
