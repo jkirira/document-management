7643728f06aace2de4cecc67bf2c0f05c5e7f085
jkirira <jkirira@cytonn.com>
2024-03-03 10:17:42 +0300

    Fix user category index and search.

--- app/Models/UserCategory.php
+++ app/Models/UserCategory.php
@@ -5,15 +5,22 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Nicolaslopezj\Searchable\SearchableTrait;
 
 class UserCategory extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory, SoftDeletes, SearchableTrait;
 
     protected $table = 'user_categories';
 
     protected $guarded = [
         'id',
+    ];
+
+    protected $searchable = [
+        'columns' => [
+            'name' => 1,
+        ],
     ];
 
     public function documents()
@@ -23,6 +30,10 @@ class UserCategory extends Model
 
     public function scopeForUser($query, User $user)
     {
+        if ($user->isAdmin()) {
+            return $query;
+        }
+
         return $query->where('user_id', $user->id);
     }
 
